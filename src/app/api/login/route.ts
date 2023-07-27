cc6ef0fb7851609b978cf2df249a12a937063989
daviszung <daviszung@gmail.com>
2023-07-26 22:54:09 -0400

    debugging

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,6 +2,7 @@ import { connectDB } from '@/db/db';
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
+    return NextResponse.json({outcome: "Login Failure"});
 
     let resBody: {[index: string]: string} = {};
 
@@ -22,3 +23,19 @@ export async function POST(req: Request) {
 
     return NextResponse.json(resBody)
 }
+    // let resBody: {[index: string]: string} = {};
+
+    // try {
+    //     const db = await connectDB();
+    //     const body = await req.json();
+    //     const { username, password } = body;
+    //     const searchForAccount = await db.collection.findOne({username: username, password: password});
+    //     if (searchForAccount) {
+    //         resBody.outcome = "Login Success"
+    //     } else {
+    //         resBody.outcome = "Login Failure"
+    //     }
+    // } catch(err) {
+    //     console.log(err, "error");
+    //     resBody.outcome = "Login Failure"
+    // }
