5c892b9a5ad95a374a12c3211dac82dbc19edfe3
daviszung <daviszung@gmail.com>
2023-07-27 08:07:46 -0400

    auto logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,9 +17,9 @@ export default function Dashboard() {
 
 	useEffect(() => {
 		const data = sessionStorage.getItem("username");
-		// if (!data && !username) {
-		// 	logout();
-		// }
+		if (!data && !username) {
+			logout();
+		}
 		setUsername(data);
 	}, []);
 
